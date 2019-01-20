1553fc7989e70a54a668aa4bd8456f0ced6880f1
Superman <ndurepo1@gmail.com>
2019-01-20 14:54:56 -0500

    updated

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,3 @@
 $("li").click(function () {
-    if($(this).css("color") === "rgb(128, 128, 128)") {
-$(this).css("color", "black");
-    }
-    else {
-    $(this).css({
-color: "gray",
-textDecoration: "line-through"
-
-});
-    }
+  $(this).toggleClass("completed");
 });
